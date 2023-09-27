095bf87b4828cbf3daee8066d85167a80b911ea0
Jemuel Repoylo <119649793+syjem@users.noreply.github.com>
2023-09-27 22:36:19 +0800

    import fonts

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import "./index.css";
 import React from "react";
-// import App from "./App.tsx";
+import "typeface-nunito-sans";
 import Routes from "./Routes.tsx";
 import ReactDOM from "react-dom/client";
 import { ThemeProvider } from "./context/ThemeContext.tsx";
